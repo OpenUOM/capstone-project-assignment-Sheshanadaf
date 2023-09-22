ac7453b5389aba4cd0c4ed632e9ac828a91cb1e9
Sheshanadaf <sheshanhebron539@gmail.com>
2023-09-22 18:07:16 +0530

    database function changes

--- frontend/test/student-add.test.js
+++ frontend/test/student-add.test.js
@@ -6,6 +6,7 @@ fixture`Testing Student UI`
 
 test('Testing add students', async t => {
 
+    console.log("fsssssssssss");
     await t.navigateTo("/dbinitialize");
 
     await t.navigateTo("/addStudent");
--- frontend/test/teacher-add.test.js
+++ frontend/test/teacher-add.test.js
@@ -2,13 +2,12 @@ import {Selector} from 'testcafe';
 process.env.NODE_ENV = "test";
 
 fixture`Testing Teacher UI`
-    .page`http://localhost:8080/student`
+    .page`http://localhost:4401/`
 
 test('Testing add teachers', async t => {
     await t.navigateTo("/dbinitialize");
 
     await t.navigateTo("/addTeacher");
-    print("dvs");
     await t.typeText("#teacher-id", "123456");
     await t.typeText("#teacher-name", "Mohan Perera");
     await t.typeText("#teacher-age", "45");
